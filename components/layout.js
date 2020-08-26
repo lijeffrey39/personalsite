61334953f40c5097e47c62cbb9c08a82408d3c77
lijeffrey39 <lijeffrey39@gmail.com>
2020-08-26 11:01:58 -0400

    meta

--- components/layout.js
+++ components/layout.js
@@ -93,13 +93,11 @@ export default function Layout({ children, home, photography, album, projects, p
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="all about me"
         />
         <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={'/back.png'}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
